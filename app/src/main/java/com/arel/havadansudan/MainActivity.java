9ba69e2bd2f54078bc2fe09f8b159957fe9ee1ce
koroglu volkan <korogluvolkan@gmail.com>
2020-02-13 21:29:48 +0300

    initial commit

---
+++ app/src/main/java/com/arel/havadansudan/MainActivity.java
@@ -0,0 +1,15 @@
+package com.arel.havadansudan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundl
+                                        e savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
